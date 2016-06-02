6395ac267860bd606dc18b4abdd565f9f6792173
Nick Sehy <nicksehy@utexas.edu>
2016-06-02 16:47:43 -0500

    hey

--- EngineCore/EngineCore/Main.cpp
+++ EngineCore/EngineCore/Main.cpp
@@ -2,7 +2,7 @@
 #include "src\graphics\Window.h"
 int main()
 {
-	using namespace engineCore;
+	/*using namespace engineCore;
 	using namespace graphics;
 	Window window("Fidelio",800,600);
 	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
@@ -16,5 +16,5 @@ int main()
 		glEnd();
 		window.update();
 	}
-	return 0;
+	return 0;*/
 }
