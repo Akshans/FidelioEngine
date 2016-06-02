5cc4d127dd062fd5ee2b96183592d8c48dfcb337
Akshans <akshansv@gmail.com>
2016-06-02 15:03:56 -0500

    tches

---
+++ EngineCore/Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "src\graphics\Window.h"
+int main()
+{
+	using namespace engineCore;
+	using namespace graphics;
+	Window window("Fidelio",800,600);
+	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+	while (!window.closed())
+	{
+		window.clear();
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
+		window.update();
+	}
+	return 0;
+}
